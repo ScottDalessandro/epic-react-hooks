ff68d8044501ece1feb4a9a907fa91818a1b24a4
Scott D'Alessandro <scott@fullstackacademy.com>
2021-02-18 10:37:16 -0500

    add custom hook

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,31 +3,26 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  console.log('rendered')
-  // 🐨 initialize the state to the value from localStorage
-  const [name, setName] = React.useState(
-    /*
-      Reading from localStorage is a synchronous call and can be expensive.
-      Since this is expensive, you can pass a function to useState which is only
-      called the first time the Greeting Component is rendered, all renders after
-      the initial will by pass running useState since the argument is a function.
+/*
+  - Convention: Custom Hooks start with "use"
+  - What makes a custom hook a CH is that they use other hooks within the function, such as useState and useEffect
 
-      If you were passing a single value into useState (such as a string/number) it
-      is acceptalbe to pass a single value into useState since this is not an expensive
-      operation.
-    */
-    () => window.localStorage.getItem('name') || initialName,
+*/
+function useLocalStorage(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) || defaultValue,
   )
-
+  console.log(state)
   React.useEffect(() => {
     console.log('calling useEffect render')
     // no need to update localStorage if the name doesn't change.
-    window.localStorage.setItem('name', name)
-  }, [name, 'name']) // dependency array only re-renders useEffect when the listed
-  // dependencies are updated.
-  // React does a shallow comparasion, so object A and B will always re-render.
-
+    window.localStorage.setItem(key, state)
+  }, [state, key])
+  return [state, setState]
+}
+function Greeting({initialName = ''}) {
+  console.log('rendered')
+  const [name, setName] = useLocalStorage('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
   }
