582e95181a9e8428259c14f0cf9af99ea6e8b4ea
Scott D'Alessandro <scott@fullstackacademy.com>
2021-02-21 05:59:02 -0500

    updates

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -4,9 +4,13 @@
 import * as React from 'react'
 
 /*
-  - Convention: Custom Hooks start with "use"
-  - What makes a custom hook a CH is that they use other hooks within the function, such as useState and useEffect
-
+  useLocalStorage
+    - Acts like React.useState, except it can also save to local storage.
+    - Calls React.useState to hydarte the value (since a cb is passed to useState, it only runs on the first render)
+    - When the value updates, React.useEffect is called and updates the value in state using the setItem function. useEffect
+    also updates localStorage. 
+      - React.useEffect only exectues on the initial load and when values are updated in its dependency array
+    - All values saved to localStorage are serialized.
 */
 function useLocalStorage(
   key,
@@ -14,8 +18,11 @@ function useLocalStorage(
   {serialize = JSON.stringify, deserialize = JSON.parse} = {},
 ) {
   const [state, setState] = React.useState(() => {
+    console.log('useState invoked')
     const valueInLocalStorage = window.localStorage.getItem(key)
     if (valueInLocalStorage) {
+      console.log('DESERIALIZED')
+
       return deserialize(valueInLocalStorage) // All values are stringified, so we can grab the value in localStorage
       // and parse the value to get the proper data-type.
     }
@@ -24,18 +31,20 @@ function useLocalStorage(
     return typeof defaultValue === 'function' ? defaultValue() : defaultValue
   })
 
+  // console.log(state, setState)
+
   const prevKeyRef = React.useRef(key) // provides an object that can be mutated without triggering a re-render.
   React.useEffect(() => {
+    console.log('useEffect invoked')
     const prevKey = prevKeyRef.current
     if (prevKey !== key) {
       window.localStorage.removeItem(prevKey)
     }
     window.localStorage.setItem(key, serialize(state))
-  }, [state, key])
-  return [state, setState, serialize]
+  }, [state, key, serialize])
+  return [state, setState]
 }
 function Greeting({initialName = ''}) {
-  console.log('Greeting render')
   const [name, setName] = useLocalStorage('name', initialName)
   function handleChange(event) {
     setName(event.target.value)
